6a98337795b725a0987270cb156848415cc87443
francifrog <email@example.com>
2017-10-20 11:49:08 -0400

    fixed css

--- template-parts/page/content-services.php
+++ template-parts/page/content-services.php
@@ -1,97 +1,119 @@
 <div class="jumbotron container-fluid">
-        <h1>what we do</h1>
+    <h1>what we do</h1>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>system integration</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            The complexity of running on multiple systems and technicalities that surrounds the implementation across various departments
+            and organizations have made system integration a critical part of information technology and application
+            deployment.
+            <br/> <br/> With well integrated ERP applications like SAP, Oracle, and others, it is important that organizations
+            involve experienced system integrators. Abaster’s team prides itself as a first class system integrator of
+            ERP applications. <br/> <br/> We understand the significance of a well integrated system to the business
+            of our clients.
+        </p>
     </div>
-    <div class="container-fluid service-div">
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <h3>system integration</h3>
-                <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-            </div>
-            <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                The complexity of running on multiple systems and technicalities that surrounds the implementation across various departments
-                and organizations have made system integration a critical part of information technology and application
-                deployment.
-                <br/> <br/> With well integrated ERP applications like SAP, Oracle, and others, it is important that organizations
-                involve experienced system integrators. Abaster’s team prides itself as a first class system integrator of
-                ERP applications. <br/> <br/> We understand the significance of a well integrated system to the business
-                of our clients.
-            </p>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>help desk and support</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            Our support system is characterized with excellent customer service, brilliant performance and on-time service delivery.
+            Our Client support specialists are properly trained to act timely and professionally. We honor the service
+            agreement with our clients as ewell as adhering to the turn-around, as stated in the Service Level Agreement.
+            <br/><br/> Client support requires technical expertise to diafnose issus and respond promptly.
+            At Abaster we understand this critical requirement hence; we have recruited the best brains to respond to
+            your system issues with possible quick resolution. Supporting your business to achieve your organizational
+            goal is our responsibility.
+        </p>
+    </div>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>business process management</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            In today's economy, companies must act swiftly but diligently to increase the bottom line by reducing the costs, conserve
+            cash, and out-innovate the competition, while using fewer resources. <br/><br/> Organizations
+            must constantly review its business processes to identify gaps, eliminate waste and increase investors' values.
+            Abaster's group of Process Management Consultants is accomplished in designing the best process suitable
+            to your organization. Our interest is to improve operational efficiencies to sustain profile margins for
+            our clients.
+        </p>
+    </div>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>outsourcing</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            At Abaster, we are focused on technology and IT Outsourcing so you can concentrate on your business core competencies. A
+            growing number of firms are turning to us to provide IT outsourcing, project team staffing, and resource
+            replacements. We will work to accommodate your budget in a fair and reasonable manner, while ensuring the
+            right skill set is provided.
+        </p>
+    </div>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>identity and access management</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            Our identity and access management team provides your company with the best of breed technology that prevents unauthorized
+            access to critical and sensitive data. <br/> We will work with your organization to implement
+            the best available product throughout your system landscape. Our IDM Experts will provide the advisory services
+            tailored to the needs of your organization.
+        </p>
+    </div>
+</div>
+<div class="container-fluid service-div">
+    <div class="row">
+        <div class="col-md-4 col-sm-4 img-cont-ser">
+            <h3>training</h3>
+            <img class="service-img" src="img/placeholder.jpg" />
+        </div>
+        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
+            Abaster Consultants are well experienced in delivering in-class and client-site training. Our training is tailored to our
+            clients' needs but with a strong emphasis on quality. Since the commencements of our training classes testimonials
+            have abound from our trainees, the achievement we are very proud of as a company. While we accommodate and
+            deliver as many courses as our clients require, we often concentrate on SAP and Management courses which
+            include: <br/>  
+        </p>
+        <div class="row text-muted">
+            <ul class="col-md-2 col-sm-4 service-ul">
+                <li>SAP FInance and Controlling</li>
+                <li>SAP Material Management</li>
+                <li>SAP Plant Maintenance</li>
+                <li>SAP Warehouse Management</li>
+                <li>SAP Supply Chain Management</li>
+            </ul>
+            <ul class="col-md-2 col-sm-4 service-ul">
+                <li>SAP Customer Relationship Management</li>
+                <li>SAP Business Intelligence</li>
+                <li>SAP Human Resources</li>
+                <li> SAP Production Planning</li>
+                <li>SAP ABAP</li>
+            </ul>
+            <ul class="col-md-2 col-sm-4 service-ul">
+                <li>SAP Basis and System Administration</li>
+                <li>SAP Security and Identity Management</li>
+                <li>Project Management</li>
+                <li>Business Analysis</li>
+                <li>Change Management</li>
+            </ul>
         </div>
     </div>
-    <div class="container-fluid service-div">
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <h3>help desk and support</h3>
-                <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-            </div>
-            <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                Our support system is characterized with excellent customer service, brilliant performance and on-time service delivery. 
-                Our Client support specialists are properly trained to act timely and professionally. We honor the service agreement with our clients as ewell as adhering to the turn-around, as stated in the Service Level Agreement. <br/><br/>
-                Client support requires technical expertise to diafnose issus and respond promptly. At Abaster we understand this critical requirement hence; we have recruited the best brains to respond to your system issues with possible quick resolution. 
-                Supporting your business to achieve your organizational goal is our responsibility.
-            </p>
-        </div>
-    </div>
-    <div class="container-fluid service-div">
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <h3>business process management</h3>
-                <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-            </div>
-            <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                In today's economy, companies must act swiftly but diligently to increase the bottom line by reducing the costs, conserve cash, and out-innovate the competition, while using fewer resources. <br/><br/>
-                Organizations must constantly review its business processes to identify gaps, eliminate waste and increase investors' values. Abaster's group of Process Management Consultants is accomplished in designing the best process suitable to your organization.
-                Our interest is to improve operational efficiencies to sustain profile margins for our clients.
-            </p>
-        </div>
-    </div>
-    <div class="container-fluid service-div">
-            <div class="row">
-                <div class="col-md-4 col-sm-4">
-                    <h3>outsourcing</h3>
-                    <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-                </div>
-                <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                    At Abaster, we are focused on technology and IT Outsourcing so you can concentrate on your business core competencies. A growing number of firms are turning to us to provide IT outsourcing, 
-                    project team staffing, and resource replacements. We will work to accommodate your budget in a fair and reasonable manner, while ensuring the right skill set is provided.
-                </p>
-            </div>
-        </div>
-        <div class="container-fluid service-div">
-                <div class="row">
-                    <div class="col-md-4 col-sm-4">
-                        <h3>identity and access management</h3>
-                        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-                    </div>
-                    <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                        Our identity and access management team provides your company with the best of breed technology that prevents unauthorized access to critical and sensitive data. <br/>
-                        We will work with your organization to implement the best available product throughout your system landscape. Our IDM Experts will provide the advisory services tailored to the needs of your organization.
-                    </p>
-                </div>
-            </div>
-            <div class="container-fluid service-div">
-                    <div class="row">
-                        <div class="col-md-4 col-sm-4">
-                            <h3>training</h3>
-                            <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/images/placeholder-image.jpg'; ?>" />
-                        </div>
-                        <p class="col-md-8 col-sm-8 text-muted service-p text-center">
-                            Abaster Consultants are well experienced in delivering in-class and client-site training. Our training is tailored to our clients' needs but with a strong emphasis on quality. Since the commencements of our training classes testimonials have abound from our trainees, the achievement we are very proud of as a company. While we accommodate and deliver as many courses as our clients require, we often concentrate on SAP and Management courses which include:<br/><br/>
-                            SAP FInance and Controlling <br/>
-                            SAP Material Management <br/>
-                            SAP Plant Maintenance <br/>
-                            SAP Warehouse Management <br/>
-                            SAP Supply Chain Management <br/>
-                            SAP Customer Relationship Management <br/>
-                            SAP Business Intelligence <br/>
-                            SAP Human Resources <br/>
-                            SAP Production Planning <br/>
-                            SAP ABAP <br/>
-                            SAP Basis and System Administration <br/>
-                            SAP Security and Identity Management <br/>
-                            Project Management <br/>
-                            Business Analysis <br/>
-                            Change Management <br/>
-                        </p>
-                    </div>
-                </div>
+</div>
