2f48fc0f9056bace497967060839c78de50be1a9
feier-lai <feierlai04@gmail.com>
2017-11-03 11:52:29 -0400

    landing changes, career content

--- page-templates/landing.php
+++ page-templates/landing.php
@@ -89,7 +89,7 @@ get_header(); ?>
       Our client support specialists are properly trained to act timely and professionally. We honor the service agreement with our clients as well as adhereing to the turn-around, as staated in the Service Level Agreement.<br/>
       Client support requires technical experties to diagnose issues and respond promptly. At Abaster we undrestand this critical requirement hence; we have recruited the best brains to respond to your system issues with possible quick resolution. Supporting your business to achive your organizational goal is our responsibility.<br/>
       </p>
-    <button id="LearnMore">LEARN MORE</button>
+    <a href="<?php echo get_site_url() . '/what-we-do/#helpSupport'; ?>"><button id="LearnMore">LEARN MORE</button></a>
     </div>
   </div>
 
--- template-parts/page/content-careers.php
+++ template-parts/page/content-careers.php
@@ -3,12 +3,15 @@
     </div>
     <div class="container-fluid text-center dark careers-cont about">
         <h3 class="white">why abaster</h3>
-        <p class="white contentMargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <p class="white contentMargin">Join a world class team in delivering industry standard products and services. Every member of the team is crucial to the success of our mission. 
         </p>
     </div>
     <div class="container-fluid text-center blue">
         <h3 class="white">available positions</h3>
     </div>
+    <!--<div class="container text-center spontaneous">
+        <p>None yet!</p>
+    </div>-->
     <div class="container-fluid job">
         <div class="row">
             <div class="career col-md-6">
@@ -86,5 +89,5 @@
     </div>
     <div class="container text-center spontaneous">
         <p>Interested in joining our team, but waiting for the right time or the right role? Send us your resume, your application will be taken into consideration when a job vacancy matches your profile.</p>
-        <button class="btn apply"><a target="_blanck" href="https://www.playcodemonkey.com">Apply</a></button>
+        <button class="btn apply"><a target="_blanck" href="mailto:info@abasterconsulting.com">Apply</a></button>
     </div>
